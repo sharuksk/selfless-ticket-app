a21559c02a9a29fc1c74382ecc269d98c40d7a8a
sharuksk <sharukajmal2@gmail.com>
2024-07-04 15:50:10 +0300

    one by one scanning with 2s time delay

--- src/QrScanner.jsx
+++ src/QrScanner.jsx
@@ -1,5 +1,5 @@
 // QrScanner.js
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { QrReader } from 'react-qr-reader';
 import ItemCard from './components/items';
 import './qr.scss';
@@ -8,11 +8,17 @@ const QrScanner = () => {
   const [pageStatus, setPageStatus] = useState(true);
   const [scanResults, setScanResults] = useState([]);
   const [scanned, setScanned] = useState(false);
+  const lastScanTimeRef = useRef(0);
 
   const handleResult = (result, error) => {
+    const currentTime = new Date().getTime();
+
     if (result) {
-      setScanResults(scanResults => [...scanResults, result?.text]);
-      console.log(scanResults)
+      if (currentTime - lastScanTimeRef.current > 2000) { // 2 seconds debounce
+        lastScanTimeRef.current = currentTime;
+        setScanResults(scanResults => [...scanResults, result?.text]);
+        console.log(scanResults);
+      }
     }
 
     if (error) {
@@ -29,31 +35,31 @@ const QrScanner = () => {
         </div>
       ) : (
         <div>
-          <button onClick={() => {setPageStatus(true); setScanned(true);}}>New Button</button>
+          <button onClick={() => { setPageStatus(true); setScanned(true); }}>New Button</button>
           <div className='QR-Scan-main'>
-          {!scanned && (
-            <QrReader
-              className='QR-read'
-              onResult={handleResult}
-              scanDelay={1000}
-            />
-          )}
-          <div>
-            <h3>Scanned Codes:</h3>
-            <ul>
-              {scanResults.map((result, index) => (
-                <li key={index} style={{gap: "20px"}}>
-                  <ItemCard 
-                    image="https://rukminim2.flixcart.com/image/612/612/kw85bww0/t-shirt/t/0/4/s-ts-801-803-tqh-original-imag8ycsms2ej5fz.jpeg?q=70"
-                    title={result}
-                    description="Available offers: Bank OfferGet ₹50 Instant Discount on first Flipkart UPI transaction on order of ₹200 and above. Bank Offer5% Cashback on Flipkart Axis Bank Card. Special PriceGet extra ₹6500 off (price inclusive of cashback/coupon). FreebieSpotify Premium - 3M at ₹119."
-                    price="999"
-                  />
-                </li>
-              ))}
-            </ul>
+            {!scanned && (
+              <QrReader
+                className='QR-read'
+                onResult={handleResult}
+                scanDelay={1000}
+              />
+            )}
+            <div>
+              <h3>Scanned Codes:</h3>
+              <ul>
+                {scanResults.map((result, index) => (
+                  <li key={index} style={{ gap: "20px" }}>
+                    <ItemCard 
+                      image="https://rukminim2.flixcart.com/image/612/612/kw85bww0/t-shirt/t/0/4/s-ts-801-803-tqh-original-imag8ycsms2ej5fz.jpeg?q=70"
+                      title={result}
+                      description="Available offers: Bank OfferGet ₹50 Instant Discount on first Flipkart UPI transaction on order of ₹200 and above. Bank Offer5% Cashback on Flipkart Axis Bank Card. Special PriceGet extra ₹6500 off (price inclusive of cashback/coupon). FreebieSpotify Premium - 3M at ₹119."
+                      price="999"
+                    />
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
         </div>
       )}
     </div>
