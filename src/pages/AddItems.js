04edf3a2637d9b861f0f671939f0d60ff3048eeb
sharuksk <sharukajmal2@gmail.com>
2024-07-31 17:38:38 +0300

    add item almost done

--- src/pages/AddItems.js
+++ src/pages/AddItems.js
@@ -53,91 +53,97 @@ const AddItems = () => {
   return (
 
     <>
-      <div className="flex justify-center items-center w-[920px] h-[554px] ">
-        <div className="flex-1 flex items-center justify-center h-full bg-addRect bg-no-repeat bg-left-top bg-transparent opacity-70">
-          <h1>ATLANTIS</h1>
+      <div className="flex justify-center items-center w-[920px] h-[554px]">
+        <div className="flex-1 flex items-center justify-center h-full bg-addRect backgroundOpacity-10 bg-no-repeat bg-left-top">
+          
+          <form className="flex space-y-6 flex-col items-center justify-center">
+          <h1 className="text-white font-bold text-2xl">ADD PRODUCTS</h1>
+            <div className="flex">
+              <label className="block text-slate-200 flex-none w-44 text-sm font-medium text-gray-700">
+                Product Name: 
+              </label>
+              <input
+                type="text"
+                name="itemName"
+                value={formData.itemName}
+                onChange={handleChange}
+                placeholder=" Product Name "
+                className="flex-1 bg-transparent border border-slate-300 hover:border-slate-400 rounded-lg"
+              />
+            </div>
+
+            <div className="flex">
+              <label className="block text-slate-200 flex-none w-44 text-sm font-medium text-gray-700">
+                Product ID:
+              </label>
+              <input
+                type="text"
+                name="itemId"
+                value={formData.itemId}
+                onChange={handleChange}
+                placeholder=" Product ID "
+                className="flex-1 bg-transparent border border-slate-300 hover:border-slate-400 rounded-lg"
+              />
+            </div>
+
+            <div className="flex">
+              <label className="block text-slate-200 flex-none w-44 text-sm font-medium text-gray-700">
+                Product Description:
+              </label>
+              <textarea
+                name="itemDescription"
+                value={formData.itemDescription}
+                onChange={handleChange}
+                placeholder=" Product Description "
+                className="flex-1 bg-transparent border border-slate-300 h-30 hover:border-slate-400 rounded-lg"
+              />
+            </div>
+
+            <div className="flex">
+              <label className="block text-slate-200 flex-none w-44 text-sm font-medium text-gray-700">
+                Product Price:
+              </label>
+              <input
+                type="number"
+                name="itemPrice"
+                value={formData.itemPrice}
+                onChange={handleChange}
+                placeholder=" Product Price "
+                className="flex-1 bg-transparent border border-slate-300 h-30 hover:border-slate-400 rounded-lg"
+              />
+            </div>
+
+            <div className="flex">
+              <label className="block text-slate-200 flex-none w-44 text-sm font-medium text-gray-700">
+                Product Image
+              </label>
+              <input
+                type="file"
+                name="itemImage"
+                onChange={handleChange}
+                placeholder=" Product Image "
+                className="mt-1 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
+              />
+              {/* <FileBase64
+                          className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                          onDone={(e) => {
+                              setBase(e.base64);
+                          }}
+                      />*/}
+            </div>
+
+            <div>
+              <button type="submit" className="bg-#2dd4bf" onClick={handleSubmit}>
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
         <div className="flex-none w-20 h-full">
 
         </div>
       </div>
     </>
-    // <form className="space-y-6 bg-blue-100 flex flex-col items-center justify-center">
-    //   <div>
-    //     <label className="block text-sm font-medium text-gray-700">
-    //       Item Name
-    //     </label>
-    //     <input
-    //       type="text"
-    //       name="itemName"
-    //       value={formData.itemName}
-    //       onChange={handleChange}
-    //       className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-    //     />
-    //   </div>
-
-    //   <div>
-    //     <label className="block text-sm font-medium text-gray-700">
-    //       Item ID
-    //     </label>
-    //     <input
-    //       type="text"
-    //       name="itemId"
-    //       value={formData.itemId}
-    //       onChange={handleChange}
-    //       className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-    //     />
-    //   </div>
-
-    //   <div>
-    //     <label className="block text-sm font-medium text-gray-700">
-    //       Item Description
-    //     </label>
-    //     <textarea
-    //       name="itemDescription"
-    //       value={formData.itemDescription}
-    //       onChange={handleChange}
-    //       className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-    //     />
-    //   </div>
-
-    //   <div>
-    //     <label className="block text-sm font-medium text-gray-700">
-    //       Item Price
-    //     </label>
-    //     <input
-    //       type="number"
-    //       name="itemPrice"
-    //       value={formData.itemPrice}
-    //       onChange={handleChange}
-    //       className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-    //     />
-    //   </div>
-
-    //   <div>
-    //     <label className="block text-sm font-medium text-gray-700">
-    //       Item Image
-    //     </label>
-    //     <input
-    //       type="file"
-    //       name="itemImage"
-    //       onChange={handleChange}
-    //       className="mt-1 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
-    //     />
-    //     {/* <FileBase64
-    //                 className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    //                 onDone={(e) => {
-    //                     setBase(e.base64);
-    //                 }}
-    //             />*/}
-    //   </div>
-
-    //   <div>
-    //     <button type="submit" onClick={handleSubmit}>
-    //       Submit
-    //     </button>
-    //   </div>
-    // </form>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        regular:("Regular"),
+
+      },
+      backgroundOpacity: {
+        '10': '0.1',
+        '20': '0.2',
+        '95': '0.95',
+       },
       backgroundImage: {
         'addRect': "url('./assets/addRect.png')",
+
       },
     },
   },
